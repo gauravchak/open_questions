05524da6cfa8d72def27e2d7b383fe0b10c3c856
Gaurav Chakravorty <gchak@qplum.co>
2019-03-09 21:44:25 -0500

    ~

--- hexagonal_tile_game/hexagonal_tile_game.py
+++ hexagonal_tile_game/hexagonal_tile_game.py
@@ -2,12 +2,15 @@
 Given a hexagonal tile game, where there is a letter on each tile, write an algorithm to determine
 if a given word exists by traversing the graph without repeatedly visiting nodes.
 '''
-from typing import Tuple, List #, Dict
+from typing import Tuple, List
+
 
 class WrongArgException(Exception):
     pass
 
-rev_index_map = {0:3, 1:4, 2:5, 3:0, 4:1, 5:2}
+
+rev_index_map = {0: 3, 1: 4, 2: 5, 3: 0, 4: 1, 5: 2}
+
 
 class HexGame:
     def __init__(self, tile_edges: List[Tuple[int, int, int]], tile_chars: List[str]) -> None:
@@ -21,11 +24,11 @@ class HexGame:
             # print ('MAINDEBUG set_edge {} {} {}'.format(root_node.tile_num, out_idx, child_node.tile_num))
             root_node.set_edge(out_idx, child_node)
 
-    def search_string(self, str_to_find:str) -> bool:
+    def search_string(self, str_to_find: str) -> bool:
         '''Search if the given string exists in some node traversal which does not visit the same edge twice'''
         # make a list of nodes where the first character matches
         # tmfc := tiles_matching_first_char
-        tmfc = [x for x in self.hextile_list if x.search_string(str_to_find, 0, [False]*len(self.hextile_list))]
+        tmfc = [x for x in self.hextile_list if x.search_string(str_to_find, 0, [False] * len(self.hextile_list))]
         # for each of them search the given substring after resetting the seen value of nodes
         if tmfc:
             return True
@@ -34,21 +37,23 @@ class HexGame:
 
     def print_game(self) -> None:
         for x in self.hextile_list:
-            print ('{} -> {}'.format(x.tile_char, ','.join([c.tile_char if c else '-' for c in x.out_tile_list])))
+            print('{} -> {}'.format(x.tile_char, ','.join([c.tile_char if c else '-' for c in x.out_tile_list])))
+
 
 class HexTile:
     '''
     A single tile.
     It has outgoing edges
     '''
-    def __init__(self, tile_num:int, mychar:str):
+
+    def __init__(self, tile_num: int, mychar: str):
         if len(mychar) != 1:
             raise WrongArgException
         self.tile_num = tile_num
         self.tile_char = mychar
         self.out_tile_list = [None] * 6
 
-    def set_edge(self, out_idx:int, child_node) -> None:
+    def set_edge(self, out_idx: int, child_node) -> None:
         if child_node is None:
             return
         if (out_idx < 0 or out_idx >= 6):
@@ -65,22 +70,24 @@ class HexTile:
         child_node.set_edge(rev_idx, self)
         # Set B's neighbors if mine are known
         # Set B[(3-1)%6] = A[(0+1)%6]
-        if self.out_tile_list[(out_idx+1)%6] is not None:
-            child_node.set_edge((rev_idx-1)%6, self.out_tile_list[(out_idx+1)%6])
+        if self.out_tile_list[(out_idx + 1) % 6] is not None:
+            child_node.set_edge((rev_idx - 1) % 6, self.out_tile_list[(out_idx + 1) % 6])
         # Set B[(3+1)%6] = A[(0-1)%6]
-        if self.out_tile_list[(out_idx-1)%6] is not None:
-            child_node.set_edge((rev_idx+1)%6, self.out_tile_list[(out_idx-1)%6])
+        if self.out_tile_list[(out_idx - 1) % 6] is not None:
+            child_node.set_edge((rev_idx + 1) % 6, self.out_tile_list[(out_idx - 1) % 6])
         # Set my neighbors from B's if mine are not known and B's are
         # Set A[(0+1)%6] = B[(3-1)%6]
-        if ((child_node.out_tile_list[(rev_idx-1)%6] is not None) and (self.out_tile_list[(out_idx+1)%6] is None)):
-            self.set_edge((out_idx+1)%6, child_node.out_tile_list[(rev_idx-1)%6])
+        if ((child_node.out_tile_list[(rev_idx - 1) % 6] is not None)
+                and (self.out_tile_list[(out_idx + 1) % 6] is None)):
+            self.set_edge((out_idx + 1) % 6, child_node.out_tile_list[(rev_idx - 1) % 6])
         # Set A[(0-1)%6] = B[(3+1)%6]
-        if ((child_node.out_tile_list[(rev_idx+1)%6] is not None) and (self.out_tile_list[(out_idx-1)%6] is None)):
-            self.set_edge((out_idx-1)%6, child_node.out_tile_list[(rev_idx+1)%6])
+        if ((child_node.out_tile_list[(rev_idx + 1) % 6] is not None)
+                and (self.out_tile_list[(out_idx - 1) % 6] is None)):
+            self.set_edge((out_idx - 1) % 6, child_node.out_tile_list[(rev_idx + 1) % 6])
 
-    def search_string(self, str_to_find:str, char_idx:int, seen_list:List[bool]) -> bool:
+    def search_string(self, str_to_find: str, char_idx: int, seen_list: List[bool]) -> bool:
         # If this node has been seen already then return saying no substring found that matches
-        if seen_list[self.tile_num-1]:
+        if seen_list[self.tile_num - 1]:
             return False
         # It is not expected that we call with an empty string but if could happen at root of the call
         if not str_to_find:
@@ -88,7 +95,7 @@ class HexTile:
         # This is also not expected that we call with an empty string but if could happen at root of the call
         if (char_idx >= len(str_to_find)):
             return True
-        # if this does not match the 
+        # if this does not match the
         if (self.tile_char != str_to_find[char_idx]):
             return False
         else:
@@ -98,7 +105,7 @@ class HexTile:
             else:
                 for c in self.out_tile_list:
                     if c:
-                        if c.search_string(str_to_find, char_idx+1, seen_list):
+                        if c.search_string(str_to_find, char_idx + 1, seen_list):
                             return True  # A true signal cascades up quickly
                 # if all false then set seen to false so that we can use this node in another path and return false
                 seen_list[self.tile_num - 1] = False
--- hexagonal_tile_game/main_hexagonal_tile_game.py
+++ hexagonal_tile_game/main_hexagonal_tile_game.py
@@ -6,7 +6,6 @@ from hexagonal_tile_game import find_substring
 class Test(unittest.TestCase):
     def test_simple_false(self):
         # 1, 2, 2 means node 1's 2nd out arrow goes to node 2
-        # tiles = [(1, 2, 2), (2, 3, 3), (3, 5, 4), (1, 3, 4), (4, 6, 1), (4, 1, 2), (2, 4, 4), (4, 2, 3), (2, 5, 1), (3, 6, 2)]
         tiles = [(1, 2, 2), (2, 3, 3), (3, 5, 4)]
         tile_chars = ['a', 'b', 'c', 'd']
         str_to_find = 'cab'
@@ -16,10 +15,18 @@ class Test(unittest.TestCase):
 
     def test_simple_true(self):
         # 1, 2, 2 means node 1's 2nd out arrow goes to node 2
-        # tiles = [(1, 2, 2), (2, 3, 3), (3, 5, 4), (1, 3, 4), (4, 6, 1), (4, 1, 2), (2, 4, 4), (4, 2, 3), (2, 5, 1), (3, 6, 2)]
         tiles = [(1, 2, 2), (2, 3, 3), (3, 5, 4)]
         tile_chars = ['a', 'b', 'c', 'd']
         str_to_find = 'bcd'
+        actual = find_substring(tiles, tile_chars, str_to_find)
+        expected = True
+        self.assertEqual(actual, expected)
+
+    def test_simple_true_2(self):
+        # 1, 2, 2 means node 1's 2nd out arrow goes to node 2
+        tiles = [(1, 2, 2), (2, 3, 3), (3, 5, 4)]
+        tile_chars = ['a', 'b', 'c', 'd']
+        str_to_find = 'cba'
         actual = find_substring(tiles, tile_chars, str_to_find)
         expected = True
         self.assertEqual(actual, expected)
